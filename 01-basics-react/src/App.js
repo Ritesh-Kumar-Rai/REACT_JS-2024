6b0b17272ddf2c1be2d95ca03f3e26a7fbb19156
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2024-01-31 20:06:25 +0530

    2 react folder created 1st using react 2nd using vite with basic program

---
+++ 01-basics-react/src/App.js
@@ -0,0 +1,10 @@
+
+
+function App() {
+  let name = "Ritesh Kumar Rai";
+  return (
+    <h1>Hare Krsna | {name}</h1>
+  );
+}
+
+export default App;
---
+++ 01-vite-react/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+  
+  return (
+    <h1>Hare Krsna using vite React | Ritesh Kumar Rai</h1>
+  )
+}
+
+export default App
