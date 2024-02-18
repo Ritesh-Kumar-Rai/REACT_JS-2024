ecbbcff4604c1e12a8090912030224a089307748
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2024-02-18 16:18:01 +0530

    10-todolist2, continue added useEffect, localStorage, edit and delete functionality

--- 10-todolist2/src/App.jsx
+++ 10-todolist2/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
@@ -8,21 +8,58 @@ import Navbar from './Components/Navbar';
 function App() {
   const [todo, settodo] = useState("");
   const [todos, settodos] = useState([]);
+  const [isCheck, setisCheck] = useState(false);
+
+
+  useEffect(() => {
+    const reloadtodo = JSON.parse(localStorage.getItem("todo"));
+    if(reloadtodo) settodos(reloadtodo);
+  }, [])
+  
 
   const handleChange = useCallback(
     (e) => {
-      console.log(e.target.value, uuidv4());
+      
       settodo(e.target.value);
     },
     [todo, settodo]
     );
 
+  
+    
+    useEffect(() => {
+      if(todos.length>0) localStorage.setItem("todo", JSON.stringify(todos));
+
+    }, [todos,settodos])
+    
+
     function handleSave(){
-      settodos([...todos,{id : uuidv4(), todo, isCompleted: false}]);
+      settodos([...todos,{id : uuidv4(), todo, isCompleted: false}]); // callback func.
       settodo("");
-      console.log(todos);
+    }
+
+    function handleCheck(id){
+        const index = todos.findIndex((item)=>item.id===id);
+        let shallow_todos = [...todos];
+        shallow_todos[index].isCompleted = !shallow_todos[index].isCompleted;
+        
+        settodos(shallow_todos);
+    }
+
+    function deleteTodo(index){
+        let copied_todo = [...todos];
+        copied_todo.splice(index,1);
+        settodos(copied_todo);
+        console.log(todos)
+    }
+
+    function editTodo(index,id){
+        const newTodo = todos.filter((i)=> i.id===id);
+        settodo(newTodo[0].todo);
+        deleteTodo(index);
     }
   
+
 
   return (
     <>
@@ -32,9 +69,9 @@ function App() {
           <h1 className='text-2xl mx-2 font-bold'>Add a Todo</h1>
           <div className='flex justify-between p-2 h-auto rounded-xl '>
             <input type="text" name="todolist" value={todo} onChange={handleChange} className='w-full mx-2 py-1 px-2 rounded-xl' />
-            <button type="button" className='border-none bg-violet-900 rounded-xl text-purple-100 hover:bg-purple-500 text-lg font-semibold py-1 px-2 ' onClick={handleSave}>Add</button>
+            <button type="button" className='border-none bg-violet-900 rounded-xl text-purple-100 disabled:bg-neutral-400 hover:bg-purple-500 text-lg font-semibold py-1 px-2 ' disabled={todo.length<=2} onClick={handleSave}>Add</button>
           </div>
-          <input type="checkbox" name="finished" id="finished" className='mx-2' /> 
+          <input type="checkbox" name="finished" checked={isCheck} onChange={()=>setisCheck(prev => !prev)} id="finished" className='mx-2' /> 
           <label htmlFor="finished">Completed Task</label>
           
         </div>
@@ -43,19 +80,21 @@ function App() {
           <h1 className='text-2xl mx-2 font-bold'>Your Todos :</h1>
           {todos.length === 0 ? <h1 className='mx-2 mt-3 text-zinc-500'>No Todos to Display!</h1>:""}
           {
-            todos.map(item=>{
-                  return (<div key={item.id} className="todo flex w-full justify-between items-center m-2">
+            todos.map((item,index)=>{
+               
+                return (isCheck || !item.isCompleted) && (<div key={item.id} className="todo flex w-full justify-between items-center m-2">
                     <div className='flex gap-1'>
-                        <input type="checkbox" name="" />
-                        <p className={`text-sm ${item.isCompleted ? "line-through":""}`}>{item.todo}</p>
+                        <input type="checkbox" id={item.id} value={item.isCompleted} onChange={()=>handleCheck(item.id)} />
+                        <label htmlFor={item.id} className={`text-sm ${item.isCompleted ? "line-through":""}`}>{item.todo}</label>
                     </div>
                         <div className="btns gap-1 flex md:gap-2">
-                          <button type="button" className='bg-violet-900 text-violet-100 hover:bg-violet-500 px-2 py-1 rounded-xl'><i className="ri-edit-box-line"></i></button>
-                          <button type="button" className='bg-violet-900 text-violet-100 hover:bg-violet-500 px-2 py-1 rounded-xl'><i className="ri-delete-bin-6-line"></i></button>
+                          <button type="button" className='bg-violet-900 text-violet-100 hover:bg-violet-500 px-2 py-1 rounded-xl' onClick={()=>editTodo(index,item.id)}><i className="ri-edit-box-line"></i></button>
+                          <button type="button" className='bg-violet-900 text-violet-100 hover:bg-violet-500 px-2 py-1 rounded-xl' onClick={()=>deleteTodo(index)}><i className="ri-delete-bin-6-line"></i></button>
                         </div>
                   </div>
-                  );
-            })
+                  )
+                }
+            )
           }
               
         </div>
